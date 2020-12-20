a4f5120de8c0dafd0231668664565910b16449ed
Ivan Strazov <52044670+IvanStrazov@users.noreply.github.com>
2020-12-20 10:41:46 +0300

    Update html2text.py

--- utils/html2text.py
+++ utils/html2text.py
@@ -2,6 +2,16 @@ import re
 import numpy as np
 
 def extract_text(text):
+    """
+    Extract clear text from html format.
+    
+    Parameters:
+        text (str) - text with html tags.
+    
+    Returns:
+        text (str) - clear text.
+    """
+    
     try:
         text = re.sub(r"(?:<.*?>|[^А-я\s])", " ", text).strip()
         return re.sub(r"\s+", " ", text)
